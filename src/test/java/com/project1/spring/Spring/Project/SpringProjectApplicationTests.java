1fcfd2b80c1f2543e0fc162a5d2b59d16d24d3d0
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-12-24 22:38:40 +0530

    Commit

---
+++ src/test/java/com/project1/spring/Spring/Project/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project1.spring.Spring.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
